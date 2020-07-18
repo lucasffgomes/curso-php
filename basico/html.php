5a0c2a1be309ed27434e3321ddb2e900515b9e0c
Lucas Gomes <58189878+lucasffgomes@users.noreply.github.com>
2020-07-18 12:33:14 -0300

    Initial commit

---
+++ basico/html.php
@@ -0,0 +1,10 @@
+<div class="titulo">Integração HTML</div>
+
+<h1>
+    <?php
+        echo 'Olá';
+        echo '<small>';
+        echo ' Mundo!';
+        echo '</small>';
+    ?>
+</h1>
---
+++ basico/ola.php
@@ -0,0 +1,9 @@
+<div class="titulo">Olá PHP</div>
+
+<?php 
+    echo 'Olá ';
+    echo 'Mundo!';
+?>
+
+<h2>Exemplo B</h2>
+<?= "Outras forma de me 'expressar'!" ?>
