1d1167377c2adc286c73ef6d699eee25e914e48b
Lucas Gomes <58189878+lucasffgomes@users.noreply.github.com>
2020-07-18 16:09:52 -0300

    Finalizado Módulo 1

--- basico/html.php
+++ basico/html.php
@@ -8,3 +8,8 @@
         echo '</small>';
     ?>
 </h1>
+
+<?= "<div>Outra forma de me 'expressar' !</div>" ?>
+
+<br>
+<div><button><?= "Legal" ?></button></div>
--- index.php
+++ index.php
@@ -24,6 +24,15 @@
                         <li>
                             <a href="exercicio.php?dir=basico&file=html">Integração HTML</a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=css">Integração CSS</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=desafio">Desafio</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
